58d328abe9cf196ee8bf0dcca7a39e4eba3fba06
anthony.rambaud.but3-2425-2@greta-0726.fr <anthony@DESKTOP-RJFLCF0.>
2025-06-03 09:48:57 +0200

    feat: update launcher.ps1 for scan_local_logiciel

--- launcher.ps1
+++ launcher.ps1
@@ -1,10 +1,10 @@
-# Définition des chemins des scripts
-$scanlocal = "C:\Users\Iutuser\Documents\Projet PTUT Script\postscanlocal.ps1"
+# Définition des chemins des scripts Python
+$scanlocal = "C:\Users\Iutuser\Documents\Projet PTUT Script\scan_local_logiciel.py"
 $scanreseau = "C:\Users\Iutuser\Documents\Projet PTUT Script\scan_network.py"
 
-# Exécuter le premier script PowerShell et attendre qu'il se termine
+# Exécuter le premier script Python et attendre qu'il se termine
 Write-Host "[INFO] Exécution de $scanlocal..."
-& $scanlocal
+python $scanlocal
 Write-Host "[INFO] $scanlocal terminé."
 
 # Exécuter le second script Python et attendre qu'il se termine
